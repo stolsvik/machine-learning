8d111cb6c24fab9cffca38bfcf5340dfb60ade2d
Endre Stølsvik <endre@stolsvik.com>
2017-05-02 23:13:44 +0200

    Some URLs..

--- src/main/java/com/stolsvik/neurons/Mnist.groovy
+++ src/main/java/com/stolsvik/neurons/Mnist.groovy
@@ -7,6 +7,14 @@ import java.util.function.DoubleSupplier
 /**
  * Backprop: https://mattmazur.com/2015/03/17/a-step-by-step-backpropagation-example
  *
+ * http://www.deeplearningbook.org/
+ *
+ * https://www.reddit.com/r/MachineLearning/comments/45gkoy/has_anyone_read_this_neural_networks_and_deep/
+ *
+ * http://neuralnetworksanddeeplearning.com/
+ *
+ * https://medium.com/@karpathy/yes-you-should-understand-backprop-e2f06eab496b
+ *
  * @author Endre Stølsvik, http://endre.stolsvik.com, 2017-03-14 00:16
  */
 @CompileStatic
@@ -16,7 +24,7 @@ class Mnist {
         NeuralNetwork network = new NeuralNetwork()
         network.addLayer(Layer_Input.createInputLayer(28 * 28))
 //        network.addLayer(Layer_WithInputs.createSigmoidLayer(network.layers.size(), 250, network.lastLayer.neurons))
-        network.addLayer(Layer_WithInputs.createLeakyReLULayer(network.layers.size(), 250, 0.1d, network.lastLayer.neurons))
+        network.addLayer(Layer_WithInputs.createLeakyReLULayer(network.layers.size(), 400, 0.1d, network.lastLayer.neurons))
         network.addLayer(Layer_WithInputs.createLeakyReLULayer(network.layers.size(), 250, 0.1d, network.lastLayer.neurons))
         network.addLayer(Layer_WithInputs.createLinearLayer(network.layers.size(), 10, network.lastLayer.neurons))
 
